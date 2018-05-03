4b085fa9a5c85450112df79b8ba986414e685b0d
Roland Li <roland_li@hotmail.ca>
2018-05-03 19:34:08 -0400

    Designing the project chicklet/items for the list

--- src/Components/ProjectsContainer.js
+++ src/Components/ProjectsContainer.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import {Grid,Row, Col, Clearfix, FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap'; //Everything is going to want to use grids
-
+import "./ProjectContainer.css"
 //Wireframe 1: https://wireframe.cc/6Buzm0
 
 export default class ProjectsContainer extends Component {
     constructor(props, context) {
         super(props, context);
-        this.handleChange = this.handleChange.bind(this);
+        this.handleSearchChange = this.handleSearchChange.bind(this);
     
         this.state = {
           searchText: '',
           showSearchWarning: 'False',
-          projects: [],
-          selectedProjects: []
+          projects: props.projects,
+          selectedProjects: props.projects
         };
+        
+        console.log(this.state.projects);
     } 
-    
+
+    updateSelectedProjects(){
+
+    }
+
     getValidationState() {
         //Check whether or not there are valid projects fitting this,
         //and update the selected projects list accordingly
@@ -41,12 +47,38 @@ export default class ProjectsContainer extends Component {
         }
     }
 
-    handleChange(e) {
+    handleSearchChange(e) {
         this.setState({ searchText: e.target.value });
+        this.updateSelectedProjects();
     }
     
+    // id:1,
+    // tags: [ "hackathon" , "social-good" , "website" , "team" ],
+    // languages_used: [ "C#" ],
+    // tools_used: [ "Unity", "IBM-Watson" ],
+    // date: "20170100", //YYYYMMDD. Split and convert manually for display, store like this for easy sorting
+    // title: 'bridgED',
+    // project_description: 'Real-time image analysis and language translation, combined with AR, to allow labelling of objects around the user.',
+    // work_description: '',
+    // learn_description: '',
+    // images: [ "test.png" , "test2.png" ],
+    // link_git:"",
+    // link_generic:"https://devpost.com/software/what-s-it"
     
     render (){
+        const projectObjects = this.state.selectedProjects.map( (proj) => 
+        <Row className='itemBox'> 
+            <Col xs={3} md={3} className='itemThumbnail'>
+                <img src={require("../Project_Images/" + proj.images[0])}/>
+                <div className="imageGradient"></div>
+            </Col>
+            <Col xs={9} md={9} className='itemInfo'>
+                <Row><h3>{proj.title}</h3></Row>
+                <Row><p className='fadeOut'>{proj.project_description}</p></Row>
+            </Col>
+        </Row>
+        );
+
         return(
            <div>
             <Grid>
@@ -61,15 +93,15 @@ export default class ProjectsContainer extends Component {
                         type="text"
                         value={this.state.searchText}
                         placeholder="Filter projects via title, tags, language, etc"
-                        onChange={this.handleChange}
+                        onChange={this.handleSearchChange}
                     />
                     <FormControl.Feedback />
                     <HelpBlock>{ this.state.showSearchWarning == 'True' ? 'No projects match with your query.' : ''} </HelpBlock>
                     </FormGroup>
                 </form>
                 </Col>
-                <Col xs={6} md={4}>
-               
+                <Col xs={12} md={12}>
+                    {projectObjects}
                 </Col>
             </Row>
             </Grid>
