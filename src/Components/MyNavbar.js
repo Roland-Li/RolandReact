e97b7da8cd32a3d0396e11b6dfdb6daa6e5ff2cc
Roland Li <roland_li@hotmail.ca>
2018-05-03 00:19:52 -0400

    Cleaned up navbar; moving on to other components.

--- src/Components/MyNavbar.js
+++ src/Components/MyNavbar.js
@@ -21,8 +21,10 @@ class MyNavbar extends Component {
             <Navbar inverse collapseOnSelect className="MyNavbar">
                 <Navbar.Header>
                     <Navbar.Brand className="MyNavbar-Brand">
-                        <a href="#brand">
-                        <div id="header-p1"> Roland</div><div id="header-p2">Li.xyz</div>
+                        <a href="#MyNavbar">
+                        <div id="header-p1">Roland</div>
+                        <div id="header-p2">Li</div>
+                        <div id="header-p3">.xyz</div>
                         </a>
                     </Navbar.Brand>
                     <Navbar.Toggle />
@@ -30,26 +32,14 @@ class MyNavbar extends Component {
                 <Navbar.Collapse>
                     <Nav>
                         <NavItem eventKey={1} href="#">
-                            Link
-                </NavItem>
+                            Projects
+                        </NavItem>
                         <NavItem eventKey={2} href="#">
-                            Link
-                </NavItem>
-                        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                            <MenuItem eventKey={3.1}>Action</MenuItem>
-                            <MenuItem eventKey={3.2}>Another action</MenuItem>
-                            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                            <MenuItem divider />
-                            <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                        </NavDropdown>
-                    </Nav>
-                    <Nav pullRight>
-                        <NavItem eventKey={1} href="#">
-                            Link Right
-                </NavItem>
-                        <NavItem eventKey={2} href="#">
-                            Link Right
-                </NavItem>
+                            Resume
+                        </NavItem>
+                        <NavItem eventKey={3} href="#">
+                            Contact
+                        </NavItem>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
