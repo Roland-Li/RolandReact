6cb0febf9b84c6ba315dcbe9790c7117cd7d02fd
Roland Li <roland_li@hotmail.ca>
2018-05-06 12:21:42 -0400

    Populating data for projects
    bridgED

--- src/App.js
+++ src/App.js
@@ -25,7 +25,21 @@ class App extends Component {
         project_description: 'Real-time image analysis and language translation, combined with AR, to allow labelling of objects around the user.',
         work_description: '',
         learn_description: '',
-        images: [ "test.png" , "test2.png" ],
+        images: [ "bridgED1.png" , "test2.png" ],
+        link_git:"",
+        link_generic:"https://devpost.com/software/what-s-it"
+      },
+      {
+        id:1,
+        tags: [ "hackathon" , "social-good" , "website" , "team" ],
+        languages_used: [ "C#" ],
+        tools_used: [ "Unity", "IBM-Watson" ],
+        date: "20170100", //YYYYMMDD. Split and convert manually for display, store like this for easy sorting
+        title: 'bridgED',
+        project_description: 'Real-time image analysis and language translation, combined with AR, to allow labelling of objects around the user.',
+        work_description: '',
+        learn_description: '',
+        images: [ "bridgED1.png" , "test2.png" ],
         link_git:"",
         link_generic:"https://devpost.com/software/what-s-it"
       }
